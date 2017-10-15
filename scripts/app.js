8a195f3870265b6ee7b426820c5619d2b9c5f23a
Shenole Latimer <shenole@gmail.com>
2017-10-14 22:19:19 -0400

    Finished to-do and amenities

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,4 @@
 // Audio plays when window is loaded
-
 window.onload = function greet(){
   const audioGreet = document.getElementById('greeting');
   audioGreet.pause();
@@ -9,4 +8,32 @@ window.onload = function greet(){
 
 
 
-// Navi-bar selction actions
+// ===== Navi-bar selction actions
+const toDo = document.querySelector('#to-do-bar');
+const food = document.querySelector('#cuisine-bar');
+const amenit = document.querySelector('#amen-bar');
+
+const msn = document.querySelector('.message-text');
+
+const homePage = document.querySelector('.home-view');
+var footer = document.querySelector('.footer');
+
+toDo.addEventListener('click', function(){
+  msn.textContent = "Here is a listing of the evening’s events. Which would you like to attend?"
+  homePage.style.display = 'none';
+  toDo.classList.add('hide');
+  food.classList.add('hide');
+  amenit.classList.add('hide');
+  document.querySelector('.to-do-view').classList.add('display');
+  footer.style.display = 'block';
+})
+
+amenit.addEventListener('click', function(){
+  msn.textContent = "These accomodations are all here for you to enjoy. Select one to find out more."
+  homePage.style.display = 'none';
+  toDo.classList.add('hide');
+  food.classList.add('hide');
+  amenit.classList.add('hide');
+  document.querySelector('.amenities-view').classList.add('display');
+  footer.style.display = 'block';
+})
